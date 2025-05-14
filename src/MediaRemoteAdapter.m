761cdc178c0ca5a417d5daa22de114a6393c601e
Jonas van den Berg <jonas@vandenbe.com>
2025-05-14 18:31:35 +0200

    Move keys into separate source file

--- src/MediaRemoteAdapter.m
+++ src/MediaRemoteAdapter.m
@@ -11,17 +11,6 @@
 #import "MediaRemote.h"
 #import "MediaRemoteAdapter.h"
 
-NSString *kBundleIdentifier = @"bundleIdentifier";
-NSString *kPlaying = @"playing";
-NSString *kTitle = @"title";
-NSString *kArtist = @"artist";
-NSString *kAlbum = @"album";
-NSString *kDurationMicros = @"durationMicros";
-NSString *kElapsedTimeMicros = @"elapsedTimeMicros";
-NSString *kTimestampEpochMicros = @"timestampEpochMicros";
-NSString *kArtworkMimeType = @"artworkMimeType";
-NSString *kArtworkDataBase64 = @"artworkDataBase64";
-
 static const double INDEFINITELY = 1e10;
 static const double DEBOUNCE_DELAY = 0.1; // seconds
 
---
+++ src/MediaRemoteAdapterKeys.m
@@ -0,0 +1,12 @@
+#import "MediaRemoteAdapter.h"
+
+NSString *kBundleIdentifier = @"bundleIdentifier";
+NSString *kPlaying = @"playing";
+NSString *kTitle = @"title";
+NSString *kArtist = @"artist";
+NSString *kAlbum = @"album";
+NSString *kDurationMicros = @"durationMicros";
+NSString *kElapsedTimeMicros = @"elapsedTimeMicros";
+NSString *kTimestampEpochMicros = @"timestampEpochMicros";
+NSString *kArtworkMimeType = @"artworkMimeType";
+NSString *kArtworkDataBase64 = @"artworkDataBase64";
