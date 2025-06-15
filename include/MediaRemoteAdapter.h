1af927ff4c2ba0cb2b6b1a5d9535ecd28b14e02b
Jonas van den Berg <jonas@vandenbe.com>
2025-06-16 00:15:36 +0200

    Add license

--- include/MediaRemoteAdapter.h
+++ include/MediaRemoteAdapter.h
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Jonas van den Berg
+// This file is licensed under the BSD 3-Clause License.
+
 #ifndef MEDIAREMOTEADAPTER_ADAPTER_H
 #define MEDIAREMOTEADAPTER_ADAPTER_H
 
--- src/MediaRemoteAdapter.m
+++ src/MediaRemoteAdapter.m
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Jonas van den Berg
+// This file is licensed under the BSD 3-Clause License.
+
 #include <errno.h>
 #include <signal.h>
 #include <stdarg.h>
