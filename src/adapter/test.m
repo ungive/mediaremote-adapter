dcd32625bf268bdec0a4ebf37886d5e423a73c46
Alexander <33609792+Alexander5015@users.noreply.github.com>
2025-07-23 04:43:13 -0400

    refine test.m

--- src/adapter/test.m
+++ src/adapter/test.m
@@ -1,85 +1,84 @@
-// Copyright (c) 2025 Jonas van den Berg
+// Copyright (c) 2025 Alexander5015
 // This file is licensed under the BSD 3-Clause License.
 
 #import <Foundation/Foundation.h>
-
 #import "adapter/globals.h"
 #import "utility/helpers.h"
 #import "utility/NowPlayingTestClient/NowPlayingTest.h"
 
 extern void adapter_get(void);
 
-extern void _adapter_is_it_broken_yet() {
-    setenv("ADAPTER_TEST_MODE", "1", 1);
+extern void _adapter_is_it_broken_yet(void) {
+    @autoreleasepool {
+        setenv("ADAPTER_TEST_MODE", "1", 1);
 
-    NSString *helperPath = [[[NSProcessInfo processInfo] environment] objectForKey:@"NOWPLAYING_CLIENT"];
-    if (!helperPath || [helperPath length] == 0) {
+        NSString *helperPath = NSProcessInfo.processInfo.environment[@"NOWPLAYING_CLIENT"];
+        if (helperPath.length == 0) {
+            unsetenv("ADAPTER_TEST_MODE");
+            printErrf(@"NowPlayingTestClient helper path is not set");
+            printOut(@"1");
+            exit(1);
+        }
+
+        // Set up pipes for communication with the helper process
+        NSPipe *inputPipe = [NSPipe pipe];
+        NSPipe *outputPipe = [NSPipe pipe];
+
+        NSTask *nowPlayingClientHelperTask = [[NSTask alloc] init];
+        nowPlayingClientHelperTask.launchPath = helperPath;
+        nowPlayingClientHelperTask.standardInput = inputPipe;
+        nowPlayingClientHelperTask.standardOutput = outputPipe;
+
+        @try {
+            [nowPlayingClientHelperTask launch];
+        } @catch (__unused NSException *exception) {
+            unsetenv("ADAPTER_TEST_MODE");
+            printErrf(@"Exeption while trying to launch NowPlayingClient Task")
+            printOut(@"1");
+            exit(1);
+        }
+
+        NSFileHandle *helperInput = inputPipe.fileHandleForWriting;
+        NSFileHandle *helperOutput = outputPipe.fileHandleForReading;
+
+        // Wait for setup signal from helper
+        NSData *setupData = [helperOutput availableData];
+        NSString *setupMsg = [[NSString alloc] initWithData:setupData encoding:NSUTF8StringEncoding];
+        if (![setupMsg containsString:@"setup_done"]) {
+            printErrf(@"NowPlayingTestClient did not signal setup_done");
+            [nowPlayingClientHelperTask terminate];
+            unsetenv("ADAPTER_TEST_MODE");
+            printOut(@"1");
+            exit(1);
+        }
+
+        // Capture stdout from adapter_get
+        NSPipe *stdoutPipe = [NSPipe pipe];
+        int originalStdout = dup(STDOUT_FILENO);
+        dup2(stdoutPipe.fileHandleForWriting.fileDescriptor, STDOUT_FILENO);
+
+        adapter_get();
+
+        fflush(stdout);
+        dup2(originalStdout, STDOUT_FILENO);
+        close(originalStdout);
+        [stdoutPipe.fileHandleForWriting closeFile];
+
+        NSData *adapterData = [stdoutPipe.fileHandleForReading readDataToEndOfFile];
+        NSString *adapterOutput = [[NSString alloc] initWithData:adapterData encoding:NSUTF8StringEncoding];
+        adapterOutput = [adapterOutput stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
+
         unsetenv("ADAPTER_TEST_MODE");
-        printOut(@"1");
-        exit(1);
-    }
 
-    NSPipe *toHelper = [NSPipe pipe];
-    NSPipe *fromHelper = [NSPipe pipe];
+        // Cleanup helper
+        [helperInput writeData:[@"cleanup\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        [helperInput closeFile];
+        [helperOutput availableData];
+        [helperOutput closeFile];
+        [nowPlayingClientHelperTask waitUntilExit];
 
-    NSTask *task = [[NSTask alloc] init];
-    @try {
-        [task setLaunchPath:helperPath];
-        [task setStandardInput:toHelper];
-        [task setStandardOutput:fromHelper];
-        [task launch];
-    } @catch (NSException *exception) {
-        unsetenv("ADAPTER_TEST_MODE");
-        printOut(@"1");
-        exit(1);
-    }
-
-    NSFileHandle *writeToHelper = [toHelper fileHandleForWriting];
-    NSFileHandle *readFromHelper = [fromHelper fileHandleForReading];
-
-    NSData *setupData = [readFromHelper availableData];
-    NSString *setupMsg = [[NSString alloc] initWithData:setupData encoding:NSUTF8StringEncoding];
-    if (![setupMsg containsString:@"setup_done"]) {
-        printErrf(@"NowPlayingTestClient did not signal setup_done");
-        [task terminate];
-        unsetenv("ADAPTER_TEST_MODE");
-        printOut(@"1");
-        exit(1);
-    }
-
-    NSPipe *pipe = [NSPipe pipe];
-    NSFileHandle *readHandle = [pipe fileHandleForReading];
-    int originalStdout = dup(STDOUT_FILENO);
-    dup2([[pipe fileHandleForWriting] fileDescriptor], STDOUT_FILENO);
-
-    adapter_get();
-
-    fflush(stdout);
-    dup2(originalStdout, STDOUT_FILENO);
-    close(originalStdout);
-
-    [[pipe fileHandleForWriting] closeFile];
-
-    NSData *data = [readHandle readDataToEndOfFile];
-    NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    output = [output stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-
-    unsetenv("ADAPTER_TEST_MODE");
-
-    NSString *cleanupMsg = @"cleanup\n";
-    [writeToHelper writeData:[cleanupMsg dataUsingEncoding:NSUTF8StringEncoding]];
-    [writeToHelper closeFile];
-
-    [readFromHelper availableData];
-    [readFromHelper closeFile];
-
-    [task waitUntilExit];
-
-    if ([output isEqualToString:@"null"]) {
-        printOut(@"1");
-        exit(0);
-    } else {
-        printOut(@"0");
-        exit(1);
+        BOOL isBroken = ![adapterOutput isEqualToString:@"null"];
+        printOut(isBroken ? @"0" : @"1");
+        exit(isBroken ? 1 : 0);
     }
 }
