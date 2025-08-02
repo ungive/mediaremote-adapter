6364654b3c1ebcc06e8b6a5d4ddccd65a1fbd7c6
Alexander <33609792+Alexander5015@users.noreply.github.com>
2025-08-02 01:47:13 -0400

    refactor get to use automatic count for async calls and implement new detection of test media

--- src/adapter/get.m
+++ src/adapter/get.m
@@ -33,116 +33,96 @@ void adapter_get() {
     NSString *now_option = getEnvOption(@"now");
     __block const bool calculate_now = now_option != nil;
 
-    id semaphore = dispatch_semaphore_create(0);
-
-    static const int expected_calls = 4;
-
-    __block int calls = 0; // thread-safe because the dispatch queue is serial.
     __block NSMutableDictionary *liveData = [NSMutableDictionary dictionary];
 
-    // Check if we're in test mode via environment variable
-    BOOL isTestMode = (getEnvOptionInt(@"ADAPTER_TEST_MODE")) ? YES : NO;
 
-    // Note that this function MUST be called by all MediaRemote callbacks
-    // below, even when invalid data is received or a recoverable error occurs.
-    void (^handle)() = ^{
-      calls += 1;
-      if (calls < expected_calls) {
-          return;
-      } else if (calls > expected_calls) {
-            printErrf(@"Received more than %d calls to handle(), received %d calls", expected_calls, calls);
-          assert(false);
-          return;
-      }
 
-      if (human_readable) {
-          makePayloadHumanReadable(liveData);
-      }
+    dispatch_group_t group = dispatch_group_create();
 
-      NSString *result = nil;
-      if (!allMandatoryPayloadKeysSet(liveData)) {
-          result = JSON_NULL;
-      } else {
-          result = serializeJsonDictionarySafe(liveData, human_readable);
-          if (!result) {
-              fail(@"Failed to serialize now playing information");
-          }
-      }
-      printOut(result);
-      dispatch_semaphore_signal(semaphore);
-    };
-
+    // PID and Bundle Identifier
+    dispatch_group_enter(group);
     g_mediaRemote.getNowPlayingApplicationPID(
         g_serialdispatchQueue, ^(int pid) {
-          if (pid == 0) {
-              handle();
-              return;
-          }
-          liveData[kMRAProcessIdentifier] = @(pid);
-          bool ok = appForPID(pid, ^(NSRunningApplication *process) {
-            if (process.bundleIdentifier != nil) {
-                liveData[kMRABundleIdentifier] = process.bundleIdentifier;
-            }
-            handle();
-          });
-          if (!ok) {
-              handle();
-          }
-        });
-
-    if(isTestMode) {
-        liveData[kMRABundleIdentifier] = SIMULATED_BUNDLE_ID;
-        handle();
-    } else {
-        g_mediaRemote.getNowPlayingApplicationPID(
-            g_serialdispatchQueue, ^(int pid) {
+            if (pid != 0) {
+                liveData[kMRAProcessIdentifier] = @(pid);
                 bool ok = appForPID(pid, ^(NSRunningApplication *process) {
-                    liveData[kMRABundleIdentifier] = process.bundleIdentifier;
-                    handle();
+                    if (process.bundleIdentifier != nil) {
+                        liveData[kMRABundleIdentifier] = process.bundleIdentifier;
+                    }
+                    dispatch_group_leave(group);
                 });
                 if (!ok) {
-                    handle();
+                    dispatch_group_leave(group);
                 }
-            });
-    }
+            } else {
+                dispatch_group_leave(group);
+            }
+        });
 
+    // Now Playing Client
+    dispatch_group_enter(group);
     g_mediaRemote.getNowPlayingClient(g_serialdispatchQueue, ^(id client) {
-      NSString *parentAppBundleID = nil;
-      if (client && [client respondsToSelector:@selector
-                            (parentApplicationBundleIdentifier)]) {
-          parentAppBundleID = [client
-              performSelector:@selector(parentApplicationBundleIdentifier)];
-      }
-      if (parentAppBundleID) {
-          liveData[kMRAParentApplicationBundleIdentifier] = parentAppBundleID;
-      }
-      handle();
+        NSString *parentAppBundleID = nil;
+        if (client && [client respondsToSelector:@selector(parentApplicationBundleIdentifier)]) {
+            parentAppBundleID = [client performSelector:@selector(parentApplicationBundleIdentifier)];
+        }
+        if (parentAppBundleID) {
+            liveData[kMRAParentApplicationBundleIdentifier] = parentAppBundleID;
+        }
+        dispatch_group_leave(group);
     });
 
+    // Is Playing
+    dispatch_group_enter(group);
     g_mediaRemote.getNowPlayingApplicationIsPlaying(
         g_serialdispatchQueue, ^(bool isPlaying) {
-          liveData[kMRAPlaying] = @(isPlaying);
-          handle();
+            liveData[kMRAPlaying] = @(isPlaying);
+            dispatch_group_leave(group);
         });
 
+
+    dispatch_group_enter(group);
     g_mediaRemote.getNowPlayingInfo(
         g_serialdispatchQueue, ^(NSDictionary *information) {
-          NSDictionary *converted = convertNowPlayingInformation(
-              information, convert_micros, calculate_now);
-          [liveData addEntriesFromDictionary:converted];
-          handle();
+            NSString *serviceIdentifier = information[@"kMRMediaRemoteNowPlayingInfoServiceIdentifier"];
+            if (!isTestMode && [serviceIdentifier isEqualToString:@"com.vandenbe.MediaRemoteAdapter.NowPlayingTestClient"]) {
+                [liveData removeAllObjects];
+                dispatch_group_leave(group);
+                return;
+            }
+            NSDictionary *converted = convertNowPlayingInformation(
+                information, convert_micros, calculate_now);
+            [liveData addEntriesFromDictionary:converted];
+            dispatch_group_leave(group);
         });
 
-    dispatch_time_t timeout =
-        dispatch_time(DISPATCH_TIME_NOW, GET_TIMEOUT_MILLIS * NSEC_PER_MSEC);
-    long result = dispatch_semaphore_wait(semaphore, timeout);
+    // Wait for all async callbacks or timeout
+    dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, GET_TIMEOUT_MILLIS * NSEC_PER_MSEC);
+    long result = dispatch_group_wait(group, timeout);
+
     if (result != 0) {
-        printErrf(@"Reading now playing information timed out "
-                  @"after %d milliseconds",
-                  GET_TIMEOUT_MILLIS);
-        exit(1);
+        printErrf(@"Reading now playing information timed out after %d milliseconds", GET_TIMEOUT_MILLIS);
+        dispatch_release(group);
+        return;
     }
-    dispatch_release(semaphore);
+
+    if (human_readable) {
+        makePayloadHumanReadable(liveData);
+    }
+
+    NSString *resultStr = nil;
+    if (!allMandatoryPayloadKeysSet(liveData)) {
+        resultStr = JSON_NULL;
+    } else {
+        resultStr = serializeJsonDictionarySafe(liveData, human_readable);
+        if (!resultStr) {
+            fail(@"Failed to serialize now playing information");
+        }
+    }
+
+    printOut(resultStr);
+
+    dispatch_release(group);
 }
 
 extern void adapter_get_env() { adapter_get(); }
