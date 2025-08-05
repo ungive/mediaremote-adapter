baf417e3f58fe4a0089de21a58812cfaa098a3b2
Alexander <33609792+Alexander5015@users.noreply.github.com>
2025-08-05 09:07:41 -0400

    Refactor adapter_get into a JSON serializer for internal_get function

--- src/adapter/get.m
+++ src/adapter/get.m
@@ -13,17 +13,8 @@
 
 #define GET_TIMEOUT_MILLIS 2000
 #define JSON_NULL @"null"
-#define SIMULATED_BUNDLE_ID @"com.example.bundle"
 
-void adapter_get() {
-
-    // Get ADAPTER_TEST_MODE as a boolean and set BOOL isTestMode
-    BOOL isTestMode = NO;
-    char *testModeEnv = getenv("ADAPTER_TEST_MODE");
-    if (testModeEnv && strcmp(testModeEnv, "0") != 0 && strlen(testModeEnv) > 0) {
-        isTestMode = YES;
-    }
-
+NSDictionary *internal_get(BOOL isTestMode) {
     NSString *micros_option = getEnvOption(@"micros");
     __block const bool convert_micros = micros_option != nil;
 
@@ -34,8 +25,7 @@ void adapter_get() {
     __block const bool calculate_now = now_option != nil;
 
     __block NSMutableDictionary *liveData = [NSMutableDictionary dictionary];
-
-
+    __block BOOL shouldAbort = NO;
 
     dispatch_group_t group = dispatch_group_create();
 
@@ -80,15 +70,13 @@ void adapter_get() {
             dispatch_group_leave(group);
         });
 
-
     dispatch_group_enter(group);
     g_mediaRemote.getNowPlayingInfo(
         g_serialdispatchQueue, ^(NSDictionary *information) {
             NSString *serviceIdentifier = information[kMRMediaRemoteNowPlayingInfoServiceIdentifier];
             if (!isTestMode && [serviceIdentifier isEqualToString:@"com.vandenbe.MediaRemoteAdapter.NowPlayingTestClient"]) {
-                [liveData removeAllObjects];
+                shouldAbort = YES;
                 dispatch_group_leave(group);
-                return;
             }
             NSDictionary *converted = convertNowPlayingInformation(
                 information, convert_micros, calculate_now);
@@ -103,15 +91,39 @@ void adapter_get() {
     if (result != 0) {
         printErrf(@"Reading now playing information timed out after %d milliseconds", GET_TIMEOUT_MILLIS);
         dispatch_release(group);
-        return;
+        return nil;
+    }
+
+    if (shouldAbort) {
+        dispatch_release(group);
+        return nil;
     }
 
     if (human_readable) {
         makePayloadHumanReadable(liveData);
     }
 
-    NSString *resultStr = nil;
     if (!allMandatoryPayloadKeysSet(liveData)) {
+        dispatch_release(group);
+        return nil;
+    }
+
+    dispatch_release(group);
+
+    return liveData;
+}
+
+void adapter_get() {
+    NSDictionary *liveData = internal_get(NO);
+
+    NSString *micros_option = getEnvOption(@"micros");
+    const bool convert_micros = micros_option != nil;
+
+    NSString *human_readable_option = getEnvOption(@"human-readable");
+    const bool human_readable = human_readable_option != nil;
+
+    NSString *resultStr = nil;
+    if (!liveData) {
         resultStr = JSON_NULL;
     } else {
         resultStr = serializeJsonDictionarySafe(liveData, human_readable);
@@ -121,8 +133,6 @@ void adapter_get() {
     }
 
     printOut(resultStr);
-
-    dispatch_release(group);
 }
 
 extern void adapter_get_env() { adapter_get(); }
--- src/adapter/test.m
+++ src/adapter/test.m
@@ -8,15 +8,28 @@
 #include <signal.h>
 
 static NSTask *nowPlayingClientHelperTask = nil;
+static NSFileHandle *helperInput = nil;
+static NSFileHandle *helperOutput = nil;
 
-void cleanup_and_exit() {
-    unsetenv("ADAPTER_TEST_MODE");
-    if (nowPlayingClientHelperTask) {
+void cleanup_helper() {
+    if (nowPlayingClientHelperTask && helperInput && helperOutput) {
+        @try {
+            [helperInput writeData:[@"cleanup\n" dataUsingEncoding:NSUTF8StringEncoding]];
+            [helperInput closeFile];
+            [helperOutput availableData];
+            [helperOutput closeFile];
+            [nowPlayingClientHelperTask waitUntilExit];
+        } @catch (__unused NSException *exception) {}
+    } else if (nowPlayingClientHelperTask) {
         @try {
             [nowPlayingClientHelperTask terminate];
             [nowPlayingClientHelperTask waitUntilExit];
         } @catch (__unused NSException *exception) {}
     }
+}
+
+void cleanup_and_exit() {
+    cleanup_helper();
     exit(1);
 }
 
@@ -29,48 +42,10 @@ extern void _adapter_is_it_broken_yet(void) {
         signal(SIGINT, handleSignal);
         signal(SIGTERM, handleSignal);
 
-        setenv("ADAPTER_TEST_MODE", "1", 1);
-
-        NSPipe *stdoutPipe = [NSPipe pipe];
-        NSMutableData *capturedData = [NSMutableData data];
-
-        // Set up reading from pipe in the background
-        dispatch_group_t readGroup = dispatch_group_create();
-        dispatch_group_enter(readGroup);
-
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            NSFileHandle *readHandle = stdoutPipe.fileHandleForReading;
-            NSData *data = [readHandle readDataToEndOfFile];
-            @synchronized(capturedData) {
-                [capturedData appendData:data];
-            }
-            dispatch_group_leave(readGroup);
-        });
-
-        int originalStdout = dup(STDOUT_FILENO);
-        dup2(stdoutPipe.fileHandleForWriting.fileDescriptor, STDOUT_FILENO);
-
-        adapter_get();
-
-        fflush(stdout);
-        dup2(originalStdout, STDOUT_FILENO);
-        close(originalStdout);
-        [stdoutPipe.fileHandleForWriting closeFile];
-
-        // Wait for all data to be read with 3-second timeout
-        dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC);
-        long result = dispatch_group_wait(readGroup, timeout);
-        if (result != 0) {
-            printErrf(@"Reading adapter output timed out after 3 seconds");
-            cleanup_and_exit();
-        }
-
-        NSString *adapterOutput = [[NSString alloc] initWithData:capturedData encoding:NSUTF8StringEncoding];
-        adapterOutput = [adapterOutput stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
-
         // If adapterOutput is not null, we know the adapter is working correctly
-        if (![adapterOutput isEqualToString:@"null"]) {
-            unsetenv("ADAPTER_TEST_MODE");
+        NSDictionary *result = internal_get(YES);
+        if (result != nil) {
+            cleanup_helper();
             exit(0);
         }
 
@@ -78,8 +53,8 @@ extern void _adapter_is_it_broken_yet(void) {
         // We only do this if adapterOutput is null to minimize the impact on other apps using the adapter
         NSString *helperPath = NSProcessInfo.processInfo.environment[@"NOWPLAYING_CLIENT"];
         if (helperPath.length == 0) {
-            unsetenv("ADAPTER_TEST_MODE");
             printErrf(@"NowPlayingTestClient helper path is not set");
+            cleanup_helper();
             exit(1);
         }
 
@@ -95,76 +70,33 @@ extern void _adapter_is_it_broken_yet(void) {
         @try {
             [nowPlayingClientHelperTask launch];
         } @catch (__unused NSException *exception) {
-            unsetenv("ADAPTER_TEST_MODE");
             printErrf(@"Exeption while trying to launch NowPlayingClient Task");
+            cleanup_helper();
             exit(1);
         }
 
-        NSFileHandle *helperInput = inputPipe.fileHandleForWriting;
-        NSFileHandle *helperOutput = outputPipe.fileHandleForReading;
+        helperInput = inputPipe.fileHandleForWriting;
+        helperOutput = outputPipe.fileHandleForReading;
 
         // Wait for setup signal from helper
         NSData *setupData = [helperOutput availableData];
         NSString *setupMsg = [[NSString alloc] initWithData:setupData encoding:NSUTF8StringEncoding];
         if (![setupMsg containsString:@"setup_done"]) {
             printErrf(@"NowPlayingTestClient did not signal setup_done");
-            [nowPlayingClientHelperTask terminate];
-            unsetenv("ADAPTER_TEST_MODE");
+            cleanup_helper();
             exit(1);
         }
-
-
-        // Repeat adapter_get with helper running
-        NSPipe *stdoutPipe2 = [NSPipe pipe];
-        NSMutableData *capturedData2 = [NSMutableData data];
-
-        // Set up reading from pipe in the background
-        dispatch_group_t readGroup2 = dispatch_group_create();
-        dispatch_group_enter(readGroup2);
-
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            NSFileHandle *readHandle2 = stdoutPipe2.fileHandleForReading;
-            NSData *data = [readHandle2 readDataToEndOfFile];
-            @synchronized(capturedData2) {
-                [capturedData2 appendData:data];
-            }
-            dispatch_group_leave(readGroup2);
-        });
-
-        int originalStdout2 = dup(STDOUT_FILENO);
-        dup2(stdoutPipe2.fileHandleForWriting.fileDescriptor, STDOUT_FILENO);
 
         // Small delay to ensure new data is available, for some reason the first call to adapter_get slows down MediaRemote?
         [NSThread sleepForTimeInterval:0.01];
 
-        adapter_get();
-
-        fflush(stdout);
-        dup2(originalStdout2, STDOUT_FILENO);
-        close(originalStdout2);
-        [stdoutPipe2.fileHandleForWriting closeFile];
-
-        // Wait for all data to be read with 3-second timeout
-        dispatch_time_t timeout2 = dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC);
-        long result2 = dispatch_group_wait(readGroup2, timeout2);
-        if (result2 != 0) {
-            printErrf(@"Reading adapter output timed out after 3 seconds");
-            cleanup_and_exit();
+        result = internal_get(YES);
+        if (result != nil) {
+            cleanup_helper();
+            exit(0);
         }
 
-        NSString *adapterOutput2 = [[NSString alloc] initWithData:capturedData2 encoding:NSUTF8StringEncoding];
-        adapterOutput2 = [adapterOutput2 stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
-
-        unsetenv("ADAPTER_TEST_MODE");
-
-        // Cleanup helper
-        [helperInput writeData:[@"cleanup\n" dataUsingEncoding:NSUTF8StringEncoding]];
-        [helperInput closeFile];
-        [helperOutput availableData];
-        [helperOutput closeFile];
-        [nowPlayingClientHelperTask waitUntilExit];
-
-        BOOL isBroken = [adapterOutput2 isEqualToString:@"null"];
-        exit(isBroken ? 1 : 0);
+        cleanup_helper();
+        exit(1);
     }
 }
